b5b9448fb8846641f756eca6d81fc145a42a9926
damdinhmanh <manhdam.198@gmail.com>
2022-08-30 23:25:38 +0700

    Implement base MVC code
    - mainloop
    - mvc
    
    Signed-off-by: damdinhmanh <manhdam.198@gmail.com>

---
+++ model/AccountDetail.java
@@ -0,0 +1,54 @@
+package model;
+
+public class AccountDetail {
+    private String accountId;
+    private String password;
+    private String role;
+
+    /**
+     * @return String return the accountId
+     */
+    public String getAccountId() {
+        return accountId;
+    }
+
+    /**
+     * @param accountId the accountId to set
+     */
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountDetail [accountId=" + accountId + ", password=" + password + ", role=" + role + "]";
+    }
+}
